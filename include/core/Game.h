d874009a234d860b0f7c574f3300288ab2f798e4
Phat Cao <phatcao2910@gmail.com>
2026-07-12 02:52:51 +0700

    build(Game) Xây dựng cửa sổ và chỉnh lại một số kiểu dữ liệu của hàm

--- include/core/Game.h
+++ include/core/Game.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "SDL2/SDL.h"
 #include "controller/CollisionSystem.h"
 #include "controller/InputHandler.h"
 #include "model/World.h"
@@ -11,17 +12,23 @@
 class Game {
 public:
     Game();
+    ~Game();
 
-    void start();
+    bool start();
     void pause();
     void resume();
     void gameLoop();
 
 private:
+    static constexpr int WINDOW_WIDTH = 800;
+    static constexpr int WINDOW_HEIGHT = 600;
+
     World world;
     std::unique_ptr<Screen> currentScreen;
     std::unique_ptr<AudioService> audioService;
     InputHandler inputHandler;
     CollisionSystem collisionSystem;
-    bool running;
+    bool running{false};
+    SDL_Window* window{nullptr};
+    SDL_Renderer* renderer{nullptr};
 };
--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -2,13 +2,65 @@
 
 Game::Game()
     : currentScreen(std::make_unique<StartScreen>()),
-      audioService(std::make_unique<SoundManager>()),
-      running(false) {}
+      audioService(std::make_unique<SoundManager>()) {}
 
-void Game::start() {}
+Game::~Game() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+bool Game::start() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("SDL initialization failed: %s", SDL_GetError());
+        return false;
+    }
+
+    window = SDL_CreateWindow(
+        "MarioEvolution",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == nullptr) {
+        SDL_Log("Window creation failed: %s", SDL_GetError());
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (renderer == nullptr) {
+        SDL_Log("Renderer creation failed: %s", SDL_GetError());
+        return false;
+    }
+
+    running = true;
+    return true;
+}
 
 void Game::pause() {}
 
 void Game::resume() {}
 
-void Game::gameLoop() {}
+void Game::gameLoop() {
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+}
