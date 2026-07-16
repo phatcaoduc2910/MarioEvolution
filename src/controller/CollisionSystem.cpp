5206bd29c0e6a73df824a7a2c723c3eedd255c63
phatcao2910 <phatcao2910@gmail.com>
2026-07-17 01:59:13 +0700

    build(CollisionSystem) Thêm phần xử lý va chạm kiểu AABB và các trường hợp va chạm

--- src/controller/CollisionSystem.cpp
+++ src/controller/CollisionSystem.cpp
@@ -2,14 +2,46 @@
 
 #include "model/Actor.h"
 #include "model/GameObject.h"
+#include "model/StaticObject.h"
 #include "model/World.h"
 
-bool CollisionSystem::check(const Actor& actor, const GameObject& object) const {
-    (void)actor;
-    (void)object;
-    return false;
+/**
+ * Kiểm tra có va chạm object hay không.
+ */
+bool CollisionSystem::check(
+    const Actor& actor,
+    const GameObject& object
+) const {
+    const Rectangle a = actor.getBounds();
+    const Rectangle b = object.getBounds();
+
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
 }
 
+/**
+ * Chia trường hợp để giải quyết va chạm.
+ */
 void CollisionSystem::resolve(World& world) {
-    (void)world;
+    Player& player = world.getPlayer();
+
+    for (const auto& object : world.getObjects()) {
+        if (object->isSolid() && check(player, *object)) {
+            object->onCollision(player);
+        }
+    }
+
+    for (const auto& actor : world.getActors()) {
+        if (!actor->isAlive()) {
+            continue;
+        }
+
+        for (const auto& object : world.getObjects()) {
+            if (object->isSolid() && check(*actor, *object)) {
+                object->onCollision(*actor);
+            }
+        }
+    }
 }
