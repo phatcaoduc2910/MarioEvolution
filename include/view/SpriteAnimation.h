01fa8b716587b5a4b68c1aba940b36915edc742e
Nguyễn Đức Toàn <nductoan1548.ord@gmail.com>
2026-07-18 19:09:53 +0700

    fix(render): căn đúng frame animation player

--- include/view/SpriteAnimation.h
+++ include/view/SpriteAnimation.h
@@ -2,20 +2,28 @@
 
 #include <SDL2/SDL.h>
 
+struct SpriteSheetLayout {
+    int originX;
+    int originY;
+    int frameWidth;
+    int frameHeight;
+    int strideX;
+    int strideY;
+};
+
 /**
  * Quản lý frame hiện tại của một dãy animation nằm ngang trên spritesheet.
  */
 class SpriteAnimation {
 public:
-    SpriteAnimation(int frameWidth, int frameHeight, int row,
+    SpriteAnimation(SpriteSheetLayout layout, int row,
                     int startCol, int frameCount, int frameDurationMs);
     void update(int deltaMs);
     void reset();
     SDL_Rect getCurrentFrame() const;
 
 private:
-    int frameWidth;
-    int frameHeight;
+    SpriteSheetLayout layout;
     int row;
     int startCol;
     int frameCount;
